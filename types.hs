7f4e7d8921db3b18c5825062051065ad96eb5cee
zheng.wang <zheng.wang@renren-inc.com>
2016-11-07 16:48:10 +0800

    添加Point类型类，并使用Point构造函数代替无意义的参数

--- types.hs
+++ types.hs
@@ -1,7 +1,8 @@
+-- Point类型，表示平面中的点
+data Point = Point Float Float deriving (Show)
 -- Shape 是类型，它的值为Circle和Rectangle
-data Shape = Circle Float Float Float | Rectangle Float Float Float Float
-    deriving (Show)
+data Shape = Circle Point Float | Rectangle Point Point deriving (Show)
 
 area :: Shape -> Float
-area (Circle _ _ r) = pi * r ^ 2
-area (Rectangle x1 y1 x2 y2) = (abs $ x2 - x1) * (abs $ y2 - y1)
+area (Circle _ r) = pi * r ^ 2
+area (Rectangle (Point x1 y1) (Point x2 y2)) = (abs $ x2 - x1) * (abs $ y2 - y1)
