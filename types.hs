d4ca1dbb668fc83ba527098e0c109f4727f309e1
zheng.wang <zheng.wang@renren-inc.com>
2016-11-07 16:19:38 +0800

    自定义类型类Shape

---
+++ types.hs
@@ -0,0 +1,7 @@
+-- Shape 是类型，它的值为Circle和Rectangle
+data Shape = Circle Float Float Float | Rectangle Float Float Float Float
+    deriving (Show)
+
+area :: Shape -> Float
+area (Circle _ _ r) = pi * r ^ 2
+area (Rectangle x1 y1 x2 y2) = (abs $ x2 - x1) * (abs $ y2 - y1)
