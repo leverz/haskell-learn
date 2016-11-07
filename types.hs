b80675528f68414b4ab7937dd60bb55b9986a27c
zheng.wang <zheng.wang@renren-inc.com>
2016-11-07 16:57:03 +0800

    添加nudge函数，用来对图形的位置进行移动

--- types.hs
+++ types.hs
@@ -6,3 +6,7 @@ data Shape = Circle Point Float | Rectangle Point Point deriving (Show)
 area :: Shape -> Float
 area (Circle _ r) = pi * r ^ 2
 area (Rectangle (Point x1 y1) (Point x2 y2)) = (abs $ x2 - x1) * (abs $ y2 - y1)
+
+nudge :: Shape -> Float -> Float -> Shape
+nudge (Circle (Point x y) r) a b = Circle (Point (x + a) (y + b)) r
+nudge (Rectangle (Point x1 y1) (Point x2 y2)) a b = Rectangle (Point (x1 + a) (y1 + b)) (Point (x2 + a) (y2 + b))
