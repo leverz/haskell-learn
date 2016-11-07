901a74b27d86405ea940d18e88fd3db7df5977b6
zheng.wang <zheng.wang@renren-inc.com>
2016-11-07 17:07:25 +0800

    添加baseCircle＆baseRect函数，用来构造位于圆心和左上角在原点的圆形和矩形

--- types.hs
+++ types.hs
@@ -10,3 +10,9 @@ area (Rectangle (Point x1 y1) (Point x2 y2)) = (abs $ x2 - x1) * (abs $ y2 - y1)
 nudge :: Shape -> Float -> Float -> Shape
 nudge (Circle (Point x y) r) a b = Circle (Point (x + a) (y + b)) r
 nudge (Rectangle (Point x1 y1) (Point x2 y2)) a b = Rectangle (Point (x1 + a) (y1 + b)) (Point (x2 + a) (y2 + b))
+
+baseCircle :: Float -> Shape
+baseCircle = Circle (Point 0 0)
+
+baseRect :: Float -> Float -> Shape
+baseRect width height = Rectangle (Point 0 0) (Point width height)
