590bcbff71690da65c02b0d1fb6ea72fd960983f
zheng.wang <zheng.wang@renren-inc.com>
2016-12-09 21:33:05 +0800

    记录语法初学

--- person.hs
+++ person.hs
@@ -8,3 +8,17 @@ data Person = Person {
     phone :: String,
     flavor :: String
 } deriving (Show)
+
+data Person2 = Person2 String String Int Float String String deriving (Show)
+
+firstName2 :: Person2 -> String
+firstName2 (Person firstName _ _ _ _ _) = firstName
+
+lastName2 :: Person -> String
+lastName2 (Person _ lastName _ _ _ _) = lastName
+
+age :: Person -> Int
+age (Person _ _ age _ _ _) = age
+
+height :: Person -> Int
+height(Person _ _ _ height _ _) = height
