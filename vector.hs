a99eedd0c54aa6e78118a3bdb83fb30943486565
zheng.wang <zheng.wang@renren-inc.com>
2016-12-10 20:38:42 +0800

    添加Vector类型 学习类型参数的使用 以及区分类型构造器和值构造器

---
+++ vector.hs
@@ -0,0 +1,13 @@
+-- 等号左侧是一个类型构造器，右侧是一个值构造器
+-- 声明函数是，必须用类型限定而不是值，要注意区分类型构造器和值构造器
+
+data Vector a = Vector a a a deriving (Show)
+
+vplus :: (Num a) => Vector a -> Vector a -> Vector a
+(Vector i j k) `vplus` (Vector l m n) = Vector (i + l) (j + m) (k + n)
+
+dotProd :: (Num a) => Vector a -> Vector a -> a
+(Vector i j k) `dotProd` (Vector l m n) = i * l + j * m + k * n
+
+vmult :: (Num a) => Vector a -> a -> Vector a
+(Vector i j k) `vmult` m = Vector (i * m) (j * m) (k * m)
