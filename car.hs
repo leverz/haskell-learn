3290050d3b50642085920c729969b9558f62c986
zheng.wang <zheng.wang@renren-inc.com>
2016-12-10 20:12:53 +0800

    添加tellCar‘ 与 tellCar进行对比 结论是： 尽量不要再data声明中加类型约束，即便看起来没问题。因为会在函数声明中留下许多无所谓的类型约束

--- car.hs
+++ car.hs
@@ -13,3 +13,7 @@ data Car2 a b c = Car2 {
 tellCar :: Car -> String
 tellCar (Car {company = c, model = m, year = y}) = 
     "This " ++ c ++ " " ++ m ++ " was made in " ++ show y
+
+tellCar' :: (Show a) => Car2 String String a -> String
+tellCar' (Car2 {company = c, model = m, year = y}) = 
+    "This " ++ c ++ " " ++ m ++ " was made in" ++ show y
