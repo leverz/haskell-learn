aa0818e61fb5bbe4de9bfcaf560111332a29b140
zheng.wang <zheng.wang@renren-inc.com>
2016-12-12 21:32:54 +0800

    添加day.hs 修改xmind

--- day.hs
+++ day.hs
@@ -1,2 +1,4 @@
 data Day = Monday | Tuesday | Wednesday | Thursday | Friday | Saturday | Sunday
     deriving (Eq, Ord, Show, Read, Bounded, Enum)
+
+data Either a b = Left a | Right b deriving(Eq, Ord, Read, Show)
