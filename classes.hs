cbd4c0f6cc9b7b1d28beafb6e8f4a6ee56d86249
zheng.wang <zheng.wang@renren-inc.com>
2017-01-03 12:33:28 +0800

    class、instance关键字学习 最小完备定义理解学习

---
+++ classes.hs
@@ -0,0 +1,20 @@
+-- 只需要声明类型类中的函数类型
+-- class Eq a where
+--     (==) :: a -> a -> Bool
+--     (/=) :: a -> a -> Bool
+--     x == y = not ( x /= y )
+--     x /= y = not ( x == y )
+
+data TrafficLight = Red | Yellow | Green
+
+instance Eq TrafficLight where
+    Red == Red = True
+    Green == Green = True
+    Yellow == Yellow = True
+    _ == _ = False
+
+
+instance Show TrafficLight where
+    show Red = "Red light"
+    show Yellow = "Yellow light"
+    show Green = "Green light"
