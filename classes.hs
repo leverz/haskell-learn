84bcdd9d0a272ff9093e5dbbe421e48fdd48cf00
zheng.wang <zheng.wang@renren-inc.com>
2017-01-05 19:02:09 +0800

    添加无名类型学习代码 完善对kind的理解，以及对类型与值得区别

--- classes.hs
+++ classes.hs
@@ -74,3 +74,16 @@ instance Functor Tree where
 --         fmap f (Right x) = Right (f x)
 --         fmap f (Left x) = Left x
 
+class Tofu t where
+    tofu :: j a -> t a j
+
+data Frank a b = Frank {frankField :: b a} deriving (Show)
+
+instance Tofu Frank where
+    tofu x = Frank x
+
+data Barry t k p = Barry {yabba :: p, dabba :: t k} deriving (Eq, Show)
+
+instance Functor (Barry a b) where
+    fmap f (Barry {yabba = x, dabba = y}) = Barry {yabba = f x, dabba = y}
+
