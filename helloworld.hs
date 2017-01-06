e495432c0454aa737b5e9f2421b4f21813f012ed
zheng.wang <zheng.wang@renren-inc.com>
2017-01-06 16:51:24 +0800

    IO操作中何时使用let，何时使用<-

--- helloworld.hs
+++ helloworld.hs
@@ -1,4 +1,5 @@
 -- main = putStrLn "Hello, World!"
+import Data.Char
 
 module Main where
 
@@ -6,3 +7,12 @@ main = do
     putStrLn "Hello, what's your name?"
     name <- getLine
     putStrLn ("Hey " ++ name ++ ", you rock!")
+
+main2 = do
+    putStrLn "what's your first name?"
+    firstName <- getLine
+    putStrLn "what's your last name?"
+    lastName <- getLine
+    let bigFirstName = map toUpper firstName
+        bigLastName = map toUpper lastName
+    putStrLn $ "hey " ++ bigFirstName ++ " " ++ bigLastName ++ ", how are you?"
