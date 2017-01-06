4f4df748491b6ccbb8f6e9c731fe2b3f65ecb4c4
zheng.wang <zheng.wang@renren-inc.com>
2017-01-06 19:11:46 +0800

    forever I/O操作测试

--- reverseWord.hs
+++ reverseWord.hs
@@ -1,21 +1,51 @@
-module Main where
+import Control.Monad
+import Data.Char
 
-main = do
-    line <- getLine
-    if null line
-        then return "Haha"
-        else do
-            putStrLn $ reverseWords line
-            main
+-- module Main where
 
--- "."可以右结合
-reverseWords :: String -> String
-reverseWords = unwords . map reverse . words
+-- main = do
+--     line <- getLine
+--     if null line
+--         then return "Haha"
+--         else do
+--             putStrLn $ reverseWords line
+--             main
 
-main2 = do
-    return ()
-    return "HAHHA"
-    line <- getLine
-    return "Blah blah blah"
-    return 4
-    putStrLn line
+-- -- "."可以右结合
+-- reverseWords :: String -> String
+-- reverseWords = unwords . map reverse . words
+
+-- main2 = do
+--     return ()
+--     return "HAHHA"
+--     line <- getLine
+--     return "Blah blah blah"
+--     return 4
+--     putStrLn line
+
+
+-- -- 用putChar 实现 putStr
+-- -- putStr :: String -> IO ()
+-- -- putStr [] = return ()
+-- -- putStr (x:xs) = do
+-- --     putChar x
+-- --     putStr xs
+
+-- main3 = do
+--     c <- getChar
+--     if c /= ' '
+--         then do
+--             putChar c
+--             main3
+--         else return ()
+
+-- main4 = do
+--     c <- getChar
+--     when (c /= ' ') $ do
+--         putChar c
+--         main4
+
+main = forever $ do
+    putStr "Give me some input: "
+    l <- getLine
+    putStrLn $ map toUpper l
