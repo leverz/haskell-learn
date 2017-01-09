b5344874b93718fd20d94833d1f6d2fb9535965d
zheng.wang <zheng.wang@renren-inc.com>
2017-01-09 15:11:58 +0800

    interact以及大写锁代码精简

--- capslocker.hs
+++ capslocker.hs
@@ -11,13 +11,26 @@ main_getContents = do
     contents <- getContents
     putStrLn (map toUpper contents)
 
-main = do
+-- 获取少于十个字符的行内容
+main_shortLinesOnly = do
     contents <- getContents
     putStr (shortLinesOnly contents)
 
+-- 未省略参数版shortLinesOnly
 shortLinesOnly :: String -> String
 shortLinesOnly input = 
     let allLines = lines input
         shortLines = filter (\line -> length line < 10) allLines
         result = unlines shortLines
     in result
+
+-- interact版 获取少于十个字符的行内容
+main = interact shortLinesOnly'
+
+-- 省略参数版shortLinesOnly
+shortLinesOnly' :: String -> String
+shortLinesOnly' = unlines . filter (\line -> length line < 10) .lines
+
+-- less code版
+main_less_code = interact $ unlines . filter ((<10) . length) . lines
+
