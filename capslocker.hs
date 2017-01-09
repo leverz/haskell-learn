b32f0b173c7870d3942eb5bc0e47af29a48a3f58
zheng.wang <zheng.wang@renren-inc.com>
2017-01-09 14:55:42 +0800

    getContents的使用

--- capslocker.hs
+++ capslocker.hs
@@ -1,6 +1,23 @@
 import Control.Monad
 import Data.Char
 
-main = forever $ do
+-- getLine版本
+main_getLine = forever $ do
     i <- getLine
     putStrLn $ map toUpper i
+
+-- getContents版本
+main_getContents = do
+    contents <- getContents
+    putStrLn (map toUpper contents)
+
+main = do
+    contents <- getContents
+    putStr (shortLinesOnly contents)
+
+shortLinesOnly :: String -> String
+shortLinesOnly input = 
+    let allLines = lines input
+        shortLines = filter (\line -> length line < 10) allLines
+        result = unlines shortLines
+    in result
