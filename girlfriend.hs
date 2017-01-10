1a27be95ea6532761a71dc36772301dfd85b3acf
zheng.wang <zheng.wang@renren-inc.com>
2017-01-10 15:40:46 +0800

    使用withFile

--- girlfriend.hs
+++ girlfriend.hs
@@ -1,7 +1,20 @@
 import System.IO
 
+-- 不适用withFile版本
 main = do
     handle <- openFile "girlfriend.txt" ReadMode
     contents <- hGetContents handle
     putStr contents
     hClose handle
+
+main_withFile = do
+    withFile "girlfriend.txt" ReadMode (\handle -> do
+        contents <- hGetContents handle
+        putStr contents)
+
+withFile' :: FilePath -> IOMode -> (Handle -> IO a) -> IO a
+withFile' path mode f = do
+    handle <- openFile path mode
+    result <- f handle
+    hClose handle
+    return result
