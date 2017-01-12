7494c3dce168d266049e40a6d72ea997b6ed6666
zheng.wang <zheng.wang@renren-inc.com>
2017-01-12 15:52:26 +0800

    随机数与输入输出混合使用

--- random_test.hs
+++ random_test.hs
@@ -1,4 +1,5 @@
 import System.Random
+import Control.Monad(when)
 
 threeCoins :: StdGen -> (Bool, Bool, Bool)
 threeCoins gen = 
@@ -10,9 +11,49 @@ threeCoins gen =
 randoms' :: (RandomGen g, Random a) => g -> [a]
 randoms' gen = let (value, newGen) = random gen in value : randoms' newGen
 
-finiteRandoms :: (RandomGen g, Random a, Num n) => n -> g -> ([a], g)
-finiteRandoms 0 gen = ([], gen)
-finiteRandoms n gen = 
-    let (value, newGen) = random gen
-        (restOfList, finalGen) = finiteRandoms (n - 1) newGen
-    in (value:restOfList, finalGen)
+main0 = do
+    gen <- getStdGen
+    putStr $ take 20 (randomRs ('a', 'z') gen)
+
+main2 = do
+    gen <- getStdGen
+    let randomChars = randomRs ('a', 'z') gen
+        (first20, rest) = splitAt 20 randomChars
+        (second20, _) = splitAt 20 rest
+    putStrLn first20
+    putStr second20
+
+main3 = do
+    gen <- getStdGen
+    putStrLn $ take 20 (randomRs ('a', 'z') gen)
+    gen' <- newStdGen
+    putStr $ take 20 (randomRs ('a', 'z') gen)
+
+main4 = do
+    gen <- getStdGen
+    askForNumber gen
+
+askForNumber :: StdGen -> IO ()
+askForNumber gen = do
+    let (randNumber, newGen) = randomR (1, 10) gen :: (Int, StdGen)
+    putStr "Which number in the range from 1 to 10 am I thinking of?"
+    numberString <- getLine
+    when (not $ null numberString) $ do
+        let number = read numberString
+        if randNumber == number
+            then putStrLn "You are correct!"
+            else putStrLn $ "Sorry, it was " ++ show randNumber
+        askForNumber newGen
+
+main5 = do
+    gen <- getStdGen
+    let (randNumber, _) = randomR (1, 10) gen :: (Int, StdGen)
+    putStr "Which number in the range from 1 to 10 am I thinking of?"
+    numberString <- getLine
+    when (not $ null numberString) $ do
+        let number = read numberString
+        if randNumber == number
+            then putStrLn "You are correct!"
+            else putStrLn $ "Sorry, it was " ++ show randNumber
+        newStdGen
+        main5
