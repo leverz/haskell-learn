c1076147ebcbdc41d0b72475bba1a5c1b647117c
zheng.wang <zheng.wang@renren-inc.com>
2017-01-18 20:07:14 +0800

    添加roadStep函数

--- fastRoad/road.hs
+++ fastRoad/road.hs
@@ -3,3 +3,17 @@ data Road = Road Int Node
 
 data Section = Section {getA :: Int, getB :: Int, getC :: Int} deriving (Show)
 type RoadSystem = [Section]
+
+data Label = A | B | C deriving (Show)
+type Path = [(Label, Int)]
+
+roadStep :: (Path, Path) -> Section -> (Path, Path)
+roadStep (pathA, pathB) (Section a b c) = 
+    let priceA = sum $ map snd pathA
+        priceB = sum $ map snd pathB
+        forwardPriceToA = priceA + a
+        crossPriceToA = priceB + b + c
+        forwardPriceToB = priceB + b
+        crossPriceToB = priceA + a + c
+        newPathToA = if forwardPriceToA <= crossPriceToA then (A, a) : pathA else (C, c) : (A, a) : pathA
+    in (newPathToA, newPathToB)
