56aa8e52fbdce69f0e681015e2adba34ca0b5c2e
wangzheng <wangzheng@shimo.im>
2017-06-24 23:04:18 +0800

    [Features] Add balanceBar.It's from book p230.

---
+++ balanceBar/balanceBar.hs
@@ -0,0 +1,9 @@
+-- book p230
+type Birds = Int
+type Pole = (Birds, Birds)
+
+landLeft :: Birds -> Pole -> Pole
+landLeft n (left, right) = (left + n, right)
+
+landRight :: Birds -> Pole -> Pole
+landRight n (left, right) = (left, right + n)
