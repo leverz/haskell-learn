cf99e695fb2d22dc5559f687235b6188870ae7a1
wangzheng <wangzheng@shimo.im>
2017-06-24 23:36:58 +0800

    [Features] New landLeft and landRight,it will tell you if the number of birds is in balance.

--- balanceBar/balanceBar.hs
+++ balanceBar/balanceBar.hs
@@ -7,3 +7,14 @@ landLeft n (left, right) = (left + n, right)
 
 landRight :: Birds -> Pole -> Pole
 landRight n (left, right) = (left, right + n)
+
+-- This code will tell you if the number of birds is in balance。
+landLeft' :: Birds -> Pole -> Maybe Pole
+landLeft' n (left, right)
+    | abs ((left + n) - right) < 4 = Just (left + n, right)
+    | otherwise = Nothing
+
+landRight' :: Birds -> Pole -> Maybe Pole
+landRight' n (left, right)
+    | abs (left - (right + n)) < 4 = Just (left, right + n)
+    |otherwise = Nothing
