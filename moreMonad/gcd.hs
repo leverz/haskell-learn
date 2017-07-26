7f77eb00076fb70c4870f7ad213cefafe7c6e542
wangzheng <wangzheng@shimo.im>
2017-07-26 21:49:36 +0800

    [Features] gcd with Monad

--- moreMonad/gcd.hs
+++ moreMonad/gcd.hs
@@ -1,5 +1,17 @@
+import Control.Monad.Writer
+
 -- 欧几里得算法 计算最大公约数
 gcd' :: Int -> Int -> Int
 gcd' a b
   | b == 0 = a
   | otherwise = gcd' b (a `mod` b)
+
+-- gcd with Monad
+gcd'' :: Int -> Int -> Writer [String] Int
+gcd'' a b
+  | b == 0 = do
+      tell ["Finished with " ++ show a]
+      return a
+  | otherwise = do
+      tell [show a ++ " mod " ++ show b ++ " = " ++ show (a `mod` b)]
+      gcd'' b (a `mod` b)
