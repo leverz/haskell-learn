eecca80247172196e5a14a94c2a22528d1d53158
wangzheng <wangzheng@shimo.im>
2017-07-26 22:17:10 +0800

    [Features] gcd reverse. An inefficient example with left combination

--- moreMonad/gcd.hs
+++ moreMonad/gcd.hs
@@ -6,7 +6,7 @@ gcd' a b
   | b == 0 = a
   | otherwise = gcd' b (a `mod` b)
 
--- gcd with Monad
+-- gcd with Monad. Right combination
 gcd'' :: Int -> Int -> Writer [String] Int
 gcd'' a b
   | b == 0 = do
@@ -15,3 +15,14 @@ gcd'' a b
   | otherwise = do
       tell [show a ++ " mod " ++ show b ++ " = " ++ show (a `mod` b)]
       gcd'' b (a `mod` b)
+
+-- gcd reverse. An inefficient way. Left combination
+gcdReverse :: Int -> Int -> Writer [String] Int
+gcdReverse a b
+  | b == 0 = do
+      tell ["Finished with " ++ show a]
+      return a
+  | otherwise = do
+      result <- gcdReverse b (a `mod` b)
+      tell [show a ++ " mod " ++ show b ++ " = " ++ show (a `mod` b)]
+      return result
