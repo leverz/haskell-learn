d30c460feb1daa4e28a1a5d0d9526bf98a945223
wangzheng <wangzheng@shimo.im>
2017-08-07 22:05:39 +0800

    feat: use diff list.
    make DiffList as a module

---
+++ MoreMonad/DiffList.hs
@@ -0,0 +1,18 @@
+module DiffList (
+  DiffList(..),
+  toDiffList,
+  fromDiffList
+  ) where
+
+-- 差分列表
+newtype DiffList a = DiffList { getDiffList :: [a] -> [a] }
+
+toDiffList :: [a] -> DiffList a
+toDiffList xs = DiffList (xs++)
+
+fromDiffList :: DiffList a -> [a]
+fromDiffList (DiffList f) = f []
+
+instance Monoid (DiffList a) where
+  mempty = DiffList (\xs -> [] ++ xs)
+  (DiffList f) `mappend` (DiffList g) = DiffList (\xs -> f (g xs))
--- moreMonad/gcd.hs
+++ moreMonad/gcd.hs
@@ -1,4 +1,5 @@
 import Control.Monad.Writer
+import DiffList
 
 -- 欧几里得算法 计算最大公约数
 gcd' :: Int -> Int -> Int
@@ -26,3 +27,13 @@ gcdReverse a b
       result <- gcdReverse b (a `mod` b)
       tell [show a ++ " mod " ++ show b ++ " = " ++ show (a `mod` b)]
       return result
+
+gcdReverse' :: Int -> Int -> Writer (DiffList String) Int
+gcdReverse' a b
+    | b == 0 = do
+        tell (toDiffList ["Finished with " ++ show a])
+        return a
+    | otherwise = do
+        result <- gcdReverse' b (a `mod` b)
+        tell (toDiffList [show a ++ " mod " ++ show b ++ " = " ++ show (a `mod` b)])
+        return result
