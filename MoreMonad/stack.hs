9edbb38c07ba16b70248759124cd50803e7f6d2c
wangzheng <wangzheng@shimo.im>
2017-08-25 16:41:39 +0800

    带状态计算的 pop 和 push

--- MoreMonad/stack.hs
+++ MoreMonad/stack.hs
@@ -1,3 +1,5 @@
+import Control.Monad.State
+
 type Stack = [Int]
 
 pop :: Stack -> (Int, Stack)
@@ -19,3 +21,27 @@ stackManip' = do
   push 3
   a <- pop
   pop
+
+pop' :: State Stack Int
+pop' = state $ \ (x:xs) -> (x, xs)
+
+push' :: Int -> State Stack ()
+push' a = state $ \xs -> ((), a:xs)
+
+stackyStack :: State Stack ()
+stackyStack = do
+  stackNow <- get
+  if stackNow == [1, 2, 3]
+    then put [8, 3, 1]
+    else put [9, 2, 1]
+
+pop'' :: State Stack Int
+pop'' = do
+  (x:xs) <- get
+  put xs
+  return x
+
+push'' :: Int -> State Stack ()
+push x = do
+  xs <- get
+  put (x:xs)
