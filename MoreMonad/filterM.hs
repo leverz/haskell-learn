1f18aaa17220409e41aa4ede754665d7529f0a1d
wangzheng <wangzheng@shimo.im>
2017-09-02 14:19:25 +0800

    feat: filterM 使用之求幂集 —— power set

--- MoreMonad/filterM.hs
+++ MoreMonad/filterM.hs
@@ -9,3 +9,8 @@ keepSmall x
   | otherwise = do
     tell [show x ++ " is too large, throwing it away"]
     return False
+
+-- 还是没太想明白 filterM 这种用法的原理，不过可以去看源码
+-- http://hackage.haskell.org/package/base-4.10.0.0/docs/src/Control.Monad.html#
+powerset :: [a] -> [[a]]
+powerset xs = filterM (\x -> [True, False]) xs
