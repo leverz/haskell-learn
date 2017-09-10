0f1f3f5632601d5a3f3413e94e556d10c6317703
wangzheng <wangzheng@shimo.im>
2017-09-10 16:54:30 +0800

    feat: new free tree

--- MoreMonad/createMonad.hs
+++ MoreMonad/createMonad.hs
@@ -5,3 +5,7 @@ newtype Prob a = Prob { getProb :: [(a, Rational)] } deriving Show
 
 instance Functor Prob where
   fmap f (Prob xs) = Prob $ map (\(x, p) -> (f x, p)) xs
+
+flatten :: Prob (Prob a) -> Prob a
+flatten (Prob xs) = Prob $ concat $ map multAll xs
+  where multAll (Prob innerxs, p) = map (\(x, r) -> (x, p*r)) innerxs
---
+++ zipper/tree.hs
@@ -0,0 +1,25 @@
+data Tree a = Empty | Node a (Tree a) (Tree a) deriving (Show)
+
+freeTree :: Tree Char
+freeTree = 
+  Node 'p'
+    (Node 'O'
+      (Node 'L'
+        (Node 'N' Empty Empty)
+        (Node 'T' Empty Empty)
+      )
+      (Node 'Y'
+        (Node 'S' Empty Empty)
+        (Node 'A' Empty Empty)
+      )
+    )
+    (Node 'L'
+      (Node 'W'
+        (Node 'C' Empty Empty)
+        (Node 'R' Empty Empty)
+      )
+      (Node 'A'
+        (Node 'A' Empty Empty)
+        (Node 'C' Empty Empty)
+      )
+    )
