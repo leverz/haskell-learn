dc849f5b52019c2691217f72ac00d7183f59b7a1
wangzheng <wangzheng@shimo.im>
2017-09-15 23:34:31 +0800

    feat: 模式匹配和递归两种方式修改树形结构

--- zipper/tree.hs
+++ zipper/tree.hs
@@ -23,3 +23,16 @@ freeTree =
         (Node 'C' Empty Empty)
       )
     )
+
+-- 匹配并修改 W 为 P
+changeTop :: Tree Char -> Tree Char
+changeTop (Node x l (Node y (Node _ m n) r)) = Node x l (Node y (Node 'P' m n) r)
+
+-- 递归式修改 W 为 P, 其实是把模式匹配中的方向抽离出来作为输入
+data Direction = L | R deriving (Show)
+type Directions = [Direction]
+
+changeTop' :: Direction -> Tree Char -> Tree Char
+changeTop' (L:ds) (Node x l r) = Node x (changeTop ds l) r
+changeTop' (R:ds) (Node x l r) = Node x l (changeTop ds r)
+changeTop' [] (Node _ l r) = Node 'P' l r
