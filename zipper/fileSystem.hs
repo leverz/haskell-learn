48d25d8f55554efcf7bc7b8110524bd674127876
wangzheng <wangzheng@shimo.im>
2017-09-16 17:31:54 +0800

    寻找文件

--- zipper/fileSystem.hs
+++ zipper/fileSystem.hs
@@ -1,3 +1,5 @@
+import Data.List (break)
+
 type Name = String
 type Data = String
 data FSItem = File Name Data | Folder Name [FSItem] deriving (Show)
@@ -23,3 +25,18 @@ myDisk =
         ]
       ]
     ]
+
+data FSCrumb = FSCrumb Name [FSItem] [FSItem] deriving (Show)
+type FSZipper = (FSItem, [FSCrumb])
+
+fsUp :: FSZipper -> FSZipper
+fsUp (item, FSCrumb name ls rs:bs) = (Folder name (ls ++ [item] ++ rs), bs)
+
+fsTo :: Name -> FSZipper -> FSZipper
+fsTo name (Folder folderName items, bs) = 
+  let (ls, item:rs) = break (nameIs name) items
+  in (item, FSCrumb folderName ls rs:bs)
+
+nameIs :: Name -> FSItem -> Bool
+nameIs name (Folder folderName _) = name == folderName
+nameIs name (File fileName _) = name == fileName
