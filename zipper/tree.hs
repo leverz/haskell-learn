233360154f811fb536c5046efad91336db92fa9b
wangzheng <wangzheng@shimo.im>
2017-09-17 12:47:46 +0800

    feat: 安全的处理二叉树

--- zipper/tree.hs
+++ zipper/tree.hs
@@ -94,3 +94,17 @@ goForward (x:xs, bs) = (xs, x:bs)
 
 goBack :: ListZipper a -> ListZipper a
 goBack (xs, b:bs) = (b:xs, bs)
+
+-- with maybe monad
+goLeft'' :: Zipper a -> Maybe (Zipper a)
+goLeft'' (Node x l r, bs) = Just (l, LeftCrumb x r:bs)
+goLeft'' (Empty, _) = Nothing
+
+goRight'' :: Zipper a -> Maybe (Zipper a)
+goRight'' (Node x l r, bs) = Just (r, RightCrumb x l:bs)
+goRight'' (Empty, _) = Nothing
+
+goUp'' :: Zipper a -> Maybe (Zipper a)
+goUp'' (t, LeftCrumb x r:bs) = Just (Node x t r, bs)
+goUp'' (t, RightCrumb x l:bs) = Just (Node x l t, bs)
+goUp'' (_, []) = Nothing
