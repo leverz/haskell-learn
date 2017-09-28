785515936e998406ee4db0a0e4a06d34df75f041
wangzheng <wangzheng@shimo.im>
2017-09-28 14:52:33 +0800

    add a type define comment.

--- dataAnalysis/json/json.hs
+++ dataAnalysis/json/json.hs
@@ -2,7 +2,7 @@
 
 import Data.Aeson
 import Control.Applicative
-import qualified Data.ByteStrings.Lazy as B
+import qualified Data.ByteString.Lazy as B
 
 -- how to use Data.Aeson: https://hackage.haskell.org/package/aeson-1.2.2.0/docs/Data-Aeson.html
 
@@ -13,6 +13,19 @@ data Mathematician = Mathematician
                      , died :: Maybe Int
                      }
 
+{--
+ -- 取出 json 中必有的 key
+ -- (.:) :: FromJSON a => Object -> Text -> Parser a
+ -- 取出 json 中可能有的 key, 如果 key 的值为 null 也会被解析成 Nothing
+ -- (.:?) :: FromJSON a => Object -> Text -> Parser (Maybe a)
+ -- 取出 json 中可能有的 key, 这个函数会试图解析 key 的值为 null 的情况
+ -- (.:!) :: FromJSON a => Object -> Text -> Parser (Maybe a)
+ -- 设置默认值, 可以配合 .:? 来使用
+ -- (.!=) :: Parser (Maybe a) -> a -> Parser a
+ --
+ -- Value 表示一个 JSON value, Parser 表示 JSON parser
+ -- parseJSON :: Value -> Parser Value
+ --}
 instance FromJSON Mathematician where
   parseJSON (Object v) = Mathematician
                          <$> (v .: "name")
@@ -23,7 +36,8 @@ instance FromJSON Mathematician where
 main :: IO()
 main = do
   input <- B.readFile "input.json"
-
+  
+  -- decode :: FromJSON a => ByteString -> Maybe a
   let mm = decode input :: Maybe Mathematician
 
   case mm of
