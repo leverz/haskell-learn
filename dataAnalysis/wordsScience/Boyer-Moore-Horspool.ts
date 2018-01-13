a4ec041b0c524993026abd75fa6ecd026000b124
wangzheng <wangzheng@shimo.im>
2018-01-14 00:06:42 +0800

    fix: termination on first match

--- dataAnalysis/wordsScience/Boyer-Moore-Horspool.ts
+++ dataAnalysis/wordsScience/Boyer-Moore-Horspool.ts
@@ -1,5 +1,15 @@
 const reverse = ([x, ...xs]) => xs.length > 0 ? [...reverse(xs), x] : [x]
 
+const findWithDefault = (index: number, target: string, content: string): number => {
+  for (let i = 0; i < content.length; i++) {
+    if (target === content[i]) {
+      index = i
+      break
+    }
+  }
+  return index 
+}
+
 const _bmh = (index: number, [p, ...revPat], content: string, pattern: string): number => {
   if (!p) {
     return index + 1
@@ -11,9 +21,13 @@ const _bmh = (index: number, [p, ...revPat], content: string, pattern: string): 
     return _bmh(index - 1, revPat, content, pattern)
   }
   const reversePattern = reverse(pattern)
-  const addIndex = reversePattern.reduce(
-    (addIndex, cur, index) => content[index] === cur && (addIndex = index), -1)
-  return _bmh(index + (addIndex > 0 ? addIndex : pattern.length), reversePattern, content, pattern)
+
+  return _bmh(
+    index + findWithDefault(reversePattern.length, content[index], reversePattern),
+    reversePattern,
+    content,
+    pattern
+  )
 }
 
 export function bmh (pattern: string, content: string): number {
