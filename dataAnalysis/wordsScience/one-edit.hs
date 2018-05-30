754f57b626a1acfe1bead96811fd06cfb62157a2
wangzheng <wangzheng@shimo.im>
2018-05-30 09:39:40 +0800

    fix: add replaces function and add note.

--- dataAnalysis/wordsScience/one-edit.hs
+++ dataAnalysis/wordsScience/one-edit.hs
@@ -3,9 +3,16 @@ import Data.List (group, sort)
 
 edits1 :: String -> [String]
 edits1 word = unique $ deletes ++ transposes ++ replaces ++ inserts
+  -- splits 从 0 开始 take 和 drop 构造 "hi" -> [("", "hi"), ("h", "i"), ("hi", "")]
+  -- "hello" -> [("", "hello"), ("h", "ello"), ("he", "llo"), ("hel", "lo"), ("hell", "o"), ("hello", "")]
   where splits = [ (take i word', drop i word') | i <- [0..length word'] ]
+        -- 删掉其中一个字母
         deletes = [ a ++ (tail b) | (a, b) <- splits, (not.null) b ]
+        -- 前后互换顺序 只更新 b 中前两个字母的顺序，然后拼接即可
         transposes = [ a ++ [b!!1] ++ [head b] ++ (drop 2 b) | (a,b) <- splits, length b > 1 ]
+        -- 替换一个字母 只从 b 中替换，然后拼接即可
+        replaces = [ a ++ [c] ++ (drop 1 b) | (a, b) <- splits, c <- alphabet, (not.null) b ]
+        -- 插入一个字母
         inserts = [ a ++ [c] ++ b | (a, b) <- splits, c <- alphabet ]
         alphabet = ['a'..'z']
         word' = map toLower word
