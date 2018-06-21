83debbf8d1341365d0e18b34ba45833442f82468
wangzheng <wangzheng@shimo.im>
2018-06-21 22:29:16 +0800

    feat: Running a cryptographic checksum on a file

---
+++ dataAnalysis/hash/fileHash.hs
@@ -0,0 +1,19 @@
+{-# LANGUAGE PackageImports #-}
+
+import "cryptohash" Crypto.Hash
+import qualified Data.ByteString as BS
+
+md5 :: BS.ByteString -> Digest MD5
+md5 bs = hash bs
+
+sha256 :: BS.ByteString -> Digest SHA256
+sha256 bs = hash bs
+
+sha512 :: BS.ByteString -> Digest SHA512
+sha512 bs = hash bs
+
+main = do
+  byteStr <- BS.readFile "hash.hs"
+  putStrLn $ "MD5: " ++ (show.md5) byteStr
+  putStrLn $ "SHA256: " ++ (show.sha256) byteStr
+  putStrLn $ "SHA512: " ++ (show.sha512) byteStr
